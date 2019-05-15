c1b453fbc431a3192aaed8bbe85a9161487f5483
nisarg2151 <nisarg.2151@gmail.com>
2019-05-14 21:33:31 -0400

    initial files for safe posting module

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,25 @@ $('#loading').show();
 
 $(window).on("load", function () {
 
+
+  (function() {
+
+    $('#live-chat header').on('click', function() {
+  
+      $('.chat').slideToggle(300, 'swing');
+      $('.chat-message-counter').fadeToggle(300, 'swing');
+  
+    });
+  
+    $('.chat-close').on('click', function(e) {
+  
+      e.preventDefault();
+      $('#live-chat').fadeOut(300);
+  
+    });
+  
+  }) ();
+
   //close loading dimmer on load
   $('#loading').hide();
   $('#content').attr('style', 'block');
