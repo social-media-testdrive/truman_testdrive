5d9497fb443e89041cba43e7ca9c70a770063858
annaspri <37249585+annaspri@users.noreply.github.com>
2020-05-01 17:35:38 -0400

    Advancedlit: initial module creation

---
+++ public2/module_js/advancedlit/advancedlit_sim.js
@@ -0,0 +1,9 @@
+const hintsList=
+[
+  {
+    hint: `Your friend Liam just posted an article that says your school is
+    closing, and it looks like many of your other friends are sharing it as
+    well. Click on the article to see what it says!`,
+    element: '#hint1'
+  }
+];
