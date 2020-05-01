d22dc90f475b2a8c84417025df4aae04d334c01e
annaspri <37249585+annaspri@users.noreply.github.com>
2020-05-01 18:14:46 -0400

    Minor code cleanup

--- public2/module_js/cyberbullying/cyberbullying_trans_script.js
+++ public2/module_js/cyberbullying/cyberbullying_trans_script.js
@@ -16,20 +16,28 @@ function startIntro(){
 
           },
           {
-
-            intro: "Some of Dylan’s classmates have started to make fun of him at school. <br>Last week, <a href='/user/memeking' target='_blank'> <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user34.jpg'/><span>Keegan Scott</span></a> secretly took a picture of Dylan’s sketchbook. He posted the photo on social media with a mean comment."
-
+            intro: `Some of Dylan’s classmates have started to make fun of him
+            at school. <br>Last week, <a href='/user/memeking' target='_blank'>
+            <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user34.jpg'/>
+            <span>Keegan Scott</span></a> secretly took a picture of Dylan’s
+            sketchbook. He posted the photo on social media with a mean
+            comment.`
           },
           {
 
-            intro: "Since then, Keegan’s friends: <a href='/user/ella_vaderrr' target='_blank'> <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user25.jpg'/><span>Ella Sroni</span></a> & <a href='/user/goaliegirl' target='_blank'> <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user29.jpg'/><span>Breana Summers</span></a> have also been making fun of Dylan on social media."
+            intro: `Since then, Keegan’s friends:
+            <a href='/user/ella_vaderrr' target='_blank'> <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user25.jpg'/>
+            <span>Ella Sroni</span></a> & <a href='/user/goaliegirl' target='_blank'> <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/user29.jpg'/>
+            <span>Breana Summers</span></a> have also been making fun of Dylan
+            on social media.`
 
           },
           {
-            intro: "Now, let’s apply what you have learned! <br>Try to <b>spot and respond to cyberbullying posts or comments</b> on the TestDrive timeline. When you get to the end of the timeline, click “Let's Continue” to move to the next part of the lesson."
-
+            intro: `Now, let’s apply what you have learned! <br>Try to <b>spot
+            and respond to cyberbullying posts or comments</b> on the TestDrive
+            timeline. When you get to the end of the timeline, click
+            “Let's Continue” to move to the next part of the lesson.`
           }
-
         ]
       });
       intro.start().onexit(function() {
--- public2/module_js/cyberbullying/cyberbullying_tutorial.js
+++ public2/module_js/cyberbullying/cyberbullying_tutorial.js
@@ -3,44 +3,54 @@ function startIntro(){
       intro.setOptions({
         steps: [
           {
-            element: document.querySelectorAll('#step1')[0],
-            intro: "You have recently seen that some people keep saying mean things about Dylan. A group of friends from school are bullying Dylan on social media.",
+            element: '#step1',
+            intro: `You have recently seen that some people keep saying mean
+            things about Dylan. A group of friends from school are bullying
+            Dylan on social media.`,
             position: 'right',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('#step1')[0],
-            intro: "<b>Cyberbullying</b> is when someone posts or shares negative things about someone else online. <br>The <b>bully</b> may use digital devices, sites, or apps. The bully often does this again and again to the same person.",
+            element: '#step1',
+            intro: `<b>Cyberbullying</b> is when someone posts or shares
+            negative things about someone else online. <br>The <b>bully</b> may
+            use digital devices, sites, or apps. The bully often does this again
+            and again to the same person.`,
             position: 'right',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('#step3.text')[0],
-            intro: "This is an example of <b>cyberbullying.</b>",
+            element: '#step3.text',
+            intro: `This is an example of <b>cyberbullying.</b>`,
             position: 'right',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('a.flag')[0],
-            intro: "One way you can act against cyberbullying is by pressing the <b>“Flag”</b> button.",
+            element: 'a.flag',
+            intro: `One way you can act against cyberbullying is by pressing the
+            <b>“Flag”</b> button.`,
             position: 'right',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('a.flag')[0],
-            intro: "<b>Flagging</b> something will report it to the website and the bullying post will be taken down. Sometimes it takes a while for the website to respond.",
+            element: 'a.flag',
+            intro: `<b>Flagging</b> something will report it to the website and
+            the bullying post will be taken down. Sometimes it takes a while for
+            the website to respond.`,
             position: 'right',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('#step7')[0],
-            intro: "Another way you can act against cyberbullying is by writing a <b>supportive comment</b> to the target.",
+            element: '#step7',
+            intro: `Another way you can act against cyberbullying is by writing
+            a <b>supportive comment</b> to the target.`,
             position: 'left',
             scrollTo: 'tooltip'
           },
           {
-            element: document.querySelectorAll('#step7')[0],
-            intro: "Another way you can act against cyberbullying is by <b>confronting the bully</b>.",
+            element: '#step7',
+            intro: `Another way you can act against cyberbullying is by
+            <b>confronting the bully</b>.`,
             position: 'left',
             scrollTo: 'tooltip'
           },
