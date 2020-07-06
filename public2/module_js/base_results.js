ceaee0da4943cabef5b4abdfe50a6a6fdc58af12
annaspri <37249585+annaspri@users.noreply.github.com>
2020-07-06 17:20:41 -0400

    All checkboxes and text responses in reflection sections are recording

--- public2/module_js/base_results.js
+++ public2/module_js/base_results.js
@@ -2,12 +2,40 @@ function recordResponses() {
   let pathArray = window.location.pathname.split('/');
   const subdirectory2 = pathArray[2];
   let actionArray = new Array();
+
   $('.reflectionPrompt').each(function(){
     let cat = new Object();
-    cat.module = subdirectory2
+    cat.modual = subdirectory2
     cat.prompt = $(this).text();
     cat.writtenResponse = $(this).closest('.ui.label').siblings('.ui.form').find('textarea').val();
-    const jqxhr = $.post("/reflection", {action: cat, _csrf: $('meta[name="csrf-token"]').attr('content')});
+    const jqxhr = $.post("/reflection", {
+      action: cat,
+      _csrf: $('meta[name="csrf-token"]').attr('content')
+    });
+    actionArray.push(jqxhr);
+  });
+
+  $('.reflectionCheckboxesPrompt').each(function(){
+    let cat = new Object();
+    cat.modual = subdirectory2
+    cat.prompt = $(this).text();
+    let checkboxInputs = 0b0;
+    let numberOfCheckboxes = 0;
+    $(this).closest('.ui.segment').find('.ui.checkbox input').each(function(){
+      numberOfCheckboxes++;
+      if ($(this).is(":checked")){
+        checkboxInputs = checkboxInputs << 1;
+        checkboxInputs++;
+      } else {
+        checkboxInputs = checkboxInputs << 1;
+      }
+    });
+    cat.numberOfCheckboxes = numberOfCheckboxes;
+    cat.checkboxResponse = checkboxInputs;
+    const jqxhr = $.post("/reflection", {
+      action: cat,
+      _csrf: $('meta[name="csrf-token"]').attr('content')
+    });
     actionArray.push(jqxhr);
   });
 
