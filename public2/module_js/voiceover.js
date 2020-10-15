5541d403275e1a63f9b20f20ce06432a55b7f355
annaspri <37249585+annaspri@users.noreply.github.com>
2020-10-15 12:19:09 -0400

    Safari fix, mod page update

--- public2/module_js/voiceover.js
+++ public2/module_js/voiceover.js
@@ -1,74 +1,72 @@
-{
-  var audioChannel = new Audio(); // the audio channel for voiceovers
-  var voiceoverSequenceCount = 0;
+const audioChannel = new Audio(); // the audio channel for voiceovers
+let voiceoverSequenceCount = 0;
 
-  function resetVoiceoverSequenceCount(){
-    voiceoverSequenceCount = 0;
-  }
+function resetVoiceoverSequenceCount(){
+  voiceoverSequenceCount = 0;
+}
 
-  function pauseVoiceover(){
-    audioChannel.pause();
-  }
+function pauseVoiceover(){
+  audioChannel.pause();
+}
 
-  function playVoiceover(audioFile, delay = 0) {
-    const pathArray = window.location.pathname.split('/');
-    const subdirectory2 = pathArray[2];
-    if (audioFile !== '') {
-      if( audioFile[voiceoverSequenceCount] !== ''){
-        if (subdirectory2 === "cyberbullying") { // TODO: remove once cyberbullying voice-overs recorded
-          audioChannel.src = `/audioFiles/${subdirectory2}/${audioFile[voiceoverSequenceCount]}`;
-        } else {
-          if(audioFile.length > 1){
-            audioChannel.src = `https://dhpd030vnpk29.cloudfront.net/voice-overs/${audioFile[voiceoverSequenceCount]}`;
-          } else if (audioFile.length === 1) {
-            audioChannel.src = `https://dhpd030vnpk29.cloudfront.net/voice-overs/${audioFile[0]}`;
+function playVoiceover(audioFile, delay = 0) {
+  const pathArray = window.location.pathname.split('/');
+  const subdirectory2 = pathArray[2];
+  if (audioFile !== '') {
+    if( audioFile[voiceoverSequenceCount] !== ''){
+      if (subdirectory2 === "cyberbullying") { // TODO: remove once cyberbullying voice-overs recorded
+        audioChannel.src = `/audioFiles/${subdirectory2}/${audioFile[voiceoverSequenceCount]}`;
+      } else {
+        if(audioFile.length > 1){
+          audioChannel.src = `https://dhpd030vnpk29.cloudfront.net/voice-overs/${audioFile[voiceoverSequenceCount]}`;
+        } else if (audioFile.length === 1) {
+          audioChannel.src = `https://dhpd030vnpk29.cloudfront.net/voice-overs/${audioFile[0]}`;
+        }
+
+      }
+      let playVoiceoverPromise = audioChannel.play();
+      if (playVoiceoverPromise !== undefined) {
+        playVoiceoverPromise.catch(error => {
+          if (error.name === 'NotAllowedError') {
+            console.log(`** Browser has determined that audio is not allowed to play yet. **`);
+          } else {
+            console.log(error)
           }
-
-        }
-        let playVoiceoverPromise = audioChannel.play();
-        if (playVoiceoverPromise !== undefined) {
-          playVoiceoverPromise.catch(error => {
-            if (error.name === 'NotAllowedError') {
-              console.log(`** Browser has determined that audio is not allowed to play yet. **`);
-            } else {
-              console.log(error)
-            }
-          });
-        }
+        });
       }
+    }
 
-      audioChannel.onended = function(){
-        voiceoverSequenceCount++;
-        if (voiceoverSequenceCount < audioFile.length){
-          setTimeout(function(){
-            playVoiceover(audioFile,delay);
-          }, delay);
-        } else {
-          resetVoiceoverSequenceCount();
-        }
+    audioChannel.onended = function(){
+      voiceoverSequenceCount++;
+      if (voiceoverSequenceCount < audioFile.length){
+        setTimeout(function(){
+          playVoiceover(audioFile,delay);
+        }, delay);
+      } else {
+        resetVoiceoverSequenceCount();
       }
-    } else {
-      pauseVoiceover();
-      console.log(`** No audio filename provided. If this is expected, then ignore this message. **`);
     }
-  };
-
-  function addVoiceovers() {
-    for (const element in voiceoverMappings) {
-      const voiceoverInfo = voiceoverMappings[element];
-      $(element).on('click', function(){
-        pauseVoiceover();
-        setTimeout( function() {
-          playVoiceover(voiceoverInfo["files"],voiceoverInfo["delay"]);
-        }, voiceoverInfo["initialDelay"]);
-      });
-    }
-  };
-
-  window.Voiceovers = {
-    resetVoiceoverSequenceCount,
-    pauseVoiceover,
-    playVoiceover,
-    addVoiceovers
+  } else {
+    pauseVoiceover();
+    console.log(`** No audio filename provided. If this is expected, then ignore this message. **`);
   }
+};
+
+function addVoiceovers() {
+  for (const element in voiceoverMappings) {
+    const voiceoverInfo = voiceoverMappings[element];
+    $(element).on('click', function(){
+      pauseVoiceover();
+      setTimeout( function() {
+        playVoiceover(voiceoverInfo["files"],voiceoverInfo["delay"]);
+      }, voiceoverInfo["initialDelay"]);
+    });
+  }
+};
+
+window.Voiceovers = {
+  resetVoiceoverSequenceCount,
+  pauseVoiceover,
+  playVoiceover,
+  addVoiceovers
 }
