c29b7f4df4eda2e5b44c72c95bca31a31b9c9bdf
annaspri <37249585+annaspri@users.noreply.github.com>
2021-02-08 13:36:35 -0500

    Voiceovers added to presentation module

--- public2/module_js/presentation/presentation_trans_script.js
+++ public2/module_js/presentation/presentation_trans_script.js
@@ -2,14 +2,18 @@ const nextPageURL = 'modual';
 
 const stepsList = [
   {
+    intro: `Click "Next" to begin!`,
+    audioFile: ['']
+  },
+  {
     intro: `Now you get to explore the TestDrive timeline! You can read and
     respond to what others have posted, or make your own posts.`,
-    audioFile: ['']
+    audioFile: ['CUSML.12.5.3.mp3']
   },
   {
     intro: `Pretend you are on a social media timeline, and that you have a
     close friend named Elena. Look out for posts from Elena.`,
-    audioFile: ['']
+    audioFile: ['CUSML.12.5.4.mp3']
   },
   {
     intro: `She has a real account <img class='ui avatar image'
@@ -18,17 +22,17 @@ const stepsList = [
     account <img class='ui avatar image' src='https://dhpd030vnpk29.cloudfront.net/profile_pictures/elenawhoprof.jpg'>
     <span> <b> elena who </b></span></a> for close friends only. You follow both
     of her accounts.`,
-    audioFile: ['']
+    audioFile: ['CUSML.12.5.5.mp3']
   },
   {
     intro: `Look through Elena’s posts. How are the posts in her two accounts
     different? Think about her audience for each.`,
-    audioFile: ['']
+    audioFile: ['CUSML.12.5.6.mp3']
   },
   {
     intro: `You can also try creating a post that is okay for a broad social
     media audience, such as your friends and family. When you get to the bottom
     of the page, click “Let's Continue” to go on.`,
-    audioFile: ['']
+    audioFile: ['CUSML.12.5.7.mp3']
   }
 ];
--- public2/module_js/presentation/presentation_tutorial.js
+++ public2/module_js/presentation/presentation_tutorial.js
@@ -2,38 +2,51 @@ const nextPageURL = 'sim';
 
 const stepsList = [
   {
-    element: document.querySelectorAll('#step3')[0],
-    intro: "People can have additional or anonymous accounts to post things that are more personal or casual for only a close group of friends to see.",
-    position:'right',
-    scrollTo:'tooltip',
+    element: '#step3',
+    intro: `Click "Next" to begin!`,
+    position: 'right',
+    scrollTo: 'tooltip',
     audioFile: ['']
   },
   {
-    element: document.querySelectorAll('#step3')[0],
-    intro: "Sometimes, people create a separate account to talk to others who are interested in the same things as they are (e.g., video games, soccer, music).",
+    element: '#step3',
+    intro: `People can have additional or anonymous accounts to post things that
+    are more personal or casual for only a close group of friends to see.`,
     position:'right',
     scrollTo:'tooltip',
-    audioFile: ['']
+    audioFile: ['CUSML.12.3.1.mp3']
   },
   {
-    element: document.querySelectorAll('#step3')[0],
-    intro: "When they do this, they might want to be anonymous since they are talking to people that they do not know very well.",
+    element: '#step3',
+    intro: `Sometimes, people create a separate account to talk to others who are
+    interested in the same things as they are (e.g., video games, soccer, music).`,
     position:'right',
     scrollTo:'tooltip',
-    audioFile: ['']
+    audioFile: ['CUSML.12.3.2.mp3']
   },
   {
-    element: document.querySelectorAll('#step4')[0],
-    intro: "Some people create fake accounts to post mean things or to cyberbully people, but it's important to be kind and respectful on social media, no matter what account you are on.",
+    element: '#step3',
+    intro: `When they do this, they might want to be anonymous since they are
+    talking to people that they do not know very well.`,
+    position:'right',
+    scrollTo:'tooltip',
+    audioFile: ['CUSML.12.3.3.mp3']
+  },
+  {
+    element: '#step4',
+    intro: `Some people create fake accounts to post mean things or to cyberbully
+    people, but it's important to be kind and respectful on social media, no
+    matter what account you are on.`,
     position:'left',
     scrollTo:'tooltip',
-    audioFile: ['']
+    audioFile: ['CUSML.12.3.4.mp3']
   },
   {
-    element: document.querySelectorAll('#step5')[0],
-    intro: "Remember, even if you don’t use your real name, people might still figure out who you are from the things you post or the friends you have.",
+    element: '#step5',
+    intro: `Remember, even if you don’t use your real name, people might still
+    figure out who you are from the things you post or the friends you have.`,
     position:'bottom',
     scrollTo:'tooltip',
-    audioFile: ['']
+    audioFile: ['CUSML.12.3.5.mp3']
   }
 ];
