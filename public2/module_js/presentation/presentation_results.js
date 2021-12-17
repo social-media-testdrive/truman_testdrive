17bafeb17687042ec9e21dabbfc40b2fb45043f9
CORNELL\wh394 <winice.hui@gmail.com>
2021-12-17 17:32:00 -0500

    Fix radio deselection bug in reflection section (presentation) module

--- public2/module_js/presentation/presentation_results.js
+++ public2/module_js/presentation/presentation_results.js
@@ -1,36 +1,54 @@
-function onPrint(){
-  if($('.results_print').hasClass('green')){
-    $(".insertPrint").empty();
-    $(".insertPrint").css('display','block');
+function onPrint() {
+    if ($('.results_print').hasClass('green')) {
+        $(".insertPrint").empty();
+        $(".insertPrint").css('display', 'block');
 
-    $(".selectPostsQuestion1").clone().appendTo(".insertPrint");
-    $(".insertPrint").append('<div class="breakHere"</div>');
+        $(".selectPostsQuestion1").clone().appendTo(".insertPrint");
+        $(".insertPrint").append('<div class="breakHere"</div>');
 
-    $(".checkOne").clone().removeClass('reflectionPromptSegment').appendTo(".insertPrint");
-    $(".radioOne").clone().removeClass('reflectionPromptSegment').appendTo(".insertPrint");
+        $(".checkOne").clone().removeClass('reflectionPromptSegment').appendTo(".insertPrint");
 
-    $(".insertPrint").append("<h4>How did you think about the audience when you created your post?</h4>");
-    var responseOne = document.getElementById("presentation_responseOne").value;
-    $(".insertPrint").append(responseOne);
+        // At the moment of cloning and appending a radio element to ".insertPrint",
+        // the new element has the same name and id of the original one. 
+        // This causes the original radio input to be unchecked. 
 
-    $(".insertPrint").append("<h4>What are the benefits of having multiple social media accounts?</h4>");
-    var responseTwo = document.getElementById("presentation_responseTwo").value;
-    $(".insertPrint").append(responseTwo);
+        // A way to avoid this is to save the original radio input
+        // and resetting it after the cloning and appending is done. 
 
-    $(".insertPrint").append("<h4>What are the drawbacks of having multiple social media accounts?</h4>");
-    var responseThree = document.getElementById("presentation_responseThree").value;
-    $(".insertPrint").append(responseThree);
+        // Find and save the radio value ('yes' or 'no') to 'radioOne' variable
+        let radioOne = $('.radioOne')
+            .closest('.ui.segment')
+            .find('.radio.checkbox input:checked').val();
 
-    window.print();
+        $(".radioOne").clone().removeClass('reflectionPromptSegment').appendTo(".insertPrint");
 
-    $(".insertPrint").css('display','none');
-  } else {
-    if($('.voiceover_reflection1').next('.reflectionPromptSegment').is(':hidden')){
-      showWarning('.startPromptsWarning');
+        $(".insertPrint").append("<h4>How did you think about the audience when you created your post?</h4>");
+        var responseOne = document.getElementById("presentation_responseOne").value;
+        $(".insertPrint").append(responseOne);
+
+        $(".insertPrint").append("<h4>What are the benefits of having multiple social media accounts?</h4>");
+        var responseTwo = document.getElementById("presentation_responseTwo").value;
+        $(".insertPrint").append(responseTwo);
+
+        $(".insertPrint").append("<h4>What are the drawbacks of having multiple social media accounts?</h4>");
+        var responseThree = document.getElementById("presentation_responseThree").value;
+        $(".insertPrint").append(responseThree);
+
+        window.print();
+
+        $(".insertPrint").css('display', 'none');
+
+        // Reset radio selection to the original radio input
+        $('.radioOne')
+            .closest('.ui.segment')
+            .find('.radio.checkbox input[value=' + radioOne + ']').prop("checked", true);
     } else {
-      showWarning('.openAllPromptsWarning');
+        if ($('.voiceover_reflection1').next('.reflectionPromptSegment').is(':hidden')) {
+            showWarning('.startPromptsWarning');
+        } else {
+            showWarning('.openAllPromptsWarning');
+        }
     }
-  }
 }
 
 // setTimeout(function(){
