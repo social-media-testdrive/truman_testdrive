00ba32d1dc09d8cf547bd6c96b580eca1d75036d
Hannah Fabian <hmf217@lehigh.edu>
2022-01-26 10:06:32 -0500

    Fixed mongo issues after merging master into esp

--- populate.js
+++ populate.js
@@ -47,7 +47,7 @@ var MongoClient = require('mongodb').MongoClient
 
 
 //var connection = mongo.connect('mongodb://127.0.0.1/test');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.PRO_MONGODB_URI, { useNewUrlParser: true });
 var db = mongoose.connection;
 mongoose.connection.on('error', (err) => {
     console.error(err);
