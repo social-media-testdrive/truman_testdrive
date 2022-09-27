0a18d9e6019579db19c60cd213e8f985d65a1d83
Shreshth Kharbanda <skhar@uw.edu>
2022-09-26 19:36:19 -0700

    package updates

--- app.js
+++ app.js
@@ -679,7 +679,7 @@ app.use(function(err, req, res, next) {
  * Start Express server.
  */
 app.listen(app.get('port'), () => {
-  //console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
+  //console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
   //console.log('  Press CTRL-C to stop\n');
 });
 
