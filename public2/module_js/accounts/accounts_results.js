97ff53eebdaa255b0362af163056c50e7c091a40
Winice Hui <wh394@cornell.edu>
2022-11-07 13:27:41 -0500

    Test: Make printing an async function

--- public2/module_js/accounts/accounts_results.js
+++ public2/module_js/accounts/accounts_results.js
@@ -1,7 +1,6 @@
-function onPrint(){
-  if($('.results_print').hasClass('green')){
+function insertPrint() {
     $(".insertPrint").empty();
-    $(".insertPrint").css('display','block');
+    $(".insertPrint").css('display', 'block');
 
     $(".insertPrint").append(`<h4>What are some features of a strong password?</h4>`);
     var responseOne = document.getElementById("accounts_responseOne").value;
@@ -10,18 +9,23 @@ function onPrint(){
     var responseTwo = document.getElementById("accounts_responseTwo").value;
     $(".insertPrint").append(responseTwo);
     $(".insertPrint").append(`<h4>What kind of information is okay to share on
-    your social media profile?</h4>`);
+  your social media profile?</h4>`);
     var responseThree = document.getElementById("accounts_responseThree").value;
     $(".insertPrint").append(responseThree);
 
-    window.print();
+}
 
-    $(".insertPrint").css('display','none');
-  } else {
-    if($('.voiceover_reflection1').next('.reflectionPromptSegment').is(':hidden')){
-      showWarning('.startPromptsWarning');
+async function onPrint() {
+    if ($('.results_print').hasClass('green')) {
+        await insertPrint();
+        window.print();
+
+        $(".insertPrint").css('display', 'none');
     } else {
-      showWarning('.openAllPromptsWarning');
+        if ($('.voiceover_reflection1').next('.reflectionPromptSegment').is(':hidden')) {
+            showWarning('.startPromptsWarning');
+        } else {
+            showWarning('.openAllPromptsWarning');
+        }
     }
-  }
 }
