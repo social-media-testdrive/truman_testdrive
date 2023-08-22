4f34acf9681b2117ff899a011f5b1dda3a5a5aee
Joss Tripoli <jct324@lehigh.edu>
2023-08-21 20:46:44 -0400

    dynamically generate opened email

--- public2/module_js/identity_theft_explore.js
+++ public2/module_js/identity_theft_explore.js
@@ -19,6 +19,90 @@ console.log(emails[0]);
 
 function showEmail(index) {
     console.log("email shown: ", index);
+
+    var email = emails[index]; // Replace with your method to fetch email data
+    
+    // Create the main container
+    var openEmailContainer = $('<div>', { class: 'openEmailContainer' });
+
+    // Create the email segment
+    var emailSegment = $('<div>', { class: 'ui padded segment' });
+
+    // Create the closeEmailArrow button
+    var closeEmailArrow = $('<button>', {
+      class: 'ui circular icon button closeEmailArrow',
+      onclick: 'closeEmail()'
+    });
+    closeEmailArrow.append($('<i>', { class: 'left arrow big icon' }));
+
+    // Create the report button
+    var reportButton = $('<button>', {
+      class: 'ui circular icon button',
+      id: 'report'
+    });
+    reportButton.append($('<i>', { class: 'bullhorn large icon' }));
+
+    // Create the ban button
+    var banButton = $('<button>', {
+      class: 'ui circular icon button',
+      id: 'ban'
+    });
+    banButton.append($('<i>', { class: 'ban large icon' }));
+
+    // Create the delete button
+    var deleteButton = $('<button>', {
+      class: 'ui circular icon button',
+      id: 'delete'
+    });
+    deleteButton.append($('<i>', { class: 'trash alternate outline large icon' }));
+
+    // Create subjectLine and inboxLabel
+    var subjectLine = $('<span>', { id: 'subjectLine' }).text(email.subject);
+    var inboxLabel = $('<div>', { class: 'ui label', id: 'inboxLabel' }).text('Inbox');
+    inboxLabel.append($('<i>', { class: 'delete icon' }));
+    subjectLine.append(inboxLabel);
+
+    // Create openDate
+    var openDate = $('<span>', { id: 'openDate' }).text(email.date);
+    var openDateHeader = $('<div>', { class: 'ui right floated header' }).append(openDate);
+
+    // Create senderInfo and emailContent
+    var senderInfo = $('<div>', { class: 'content senderInfo' });
+    var senderHeader = $('<div>', { class: 'header' }).text(email.sender);
+    var fromEmail = $('<span>', { class: 'fromEmail' }).text(email.from);
+    senderHeader.append(fromEmail);
+    senderInfo.append(senderHeader);
+    
+    var emailContent = $('<p>', { id: 'emailContent' }).text(email.content);
+
+    // Create reply button
+    var replyButton = $('<button>', {
+      class: 'ui basic button',
+      id: 'reply'
+    });
+    replyButton.append($('<i>', { class: 'reply icon' }));
+    replyButton.append('Reply');
+
+    // Append all elements to emailSegment
+    emailSegment.append(closeEmailArrow);
+    emailSegment.append(reportButton);
+    emailSegment.append(banButton);
+    emailSegment.append(deleteButton);
+    emailSegment.append($('<div>', { class: 'ui horizontal divider' }));
+    emailSegment.append(subjectLine);
+    emailSegment.append(openDateHeader);
+    emailSegment.append($('<div>', { class: 'ui horizontal divider' }));
+    emailSegment.append($('<div>', { class: 'ui icon message' }).append($('<i>', { class: 'user circle icon' }), senderInfo));
+    emailSegment.append(emailContent);
+    emailSegment.append(replyButton);
+
+    // Append emailSegment to openEmailContainer
+    openEmailContainer.append(emailSegment);
+    
+    // Append openEmailContainer to the body
+    $('.limit').append(openEmailContainer);
+
+    
 }
   
 function closeEmail() {
