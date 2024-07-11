d463c7c327b7fd0c38000f151e59f11d95417195
Joss <jct324@lehigh.edu>
2024-07-11 05:35:56 -0400

    authority page and scroll proactively tethering to current timecode

--- public/module_js/audio-player.js
+++ public/module_js/audio-player.js
@@ -1,95 +1,88 @@
-$(document).ready(function() {
-    $('audio.with-transcript').each(function() {
-        let $audio_el = $(this);
-        let source = $audio_el.find('source').attr('src');    
-        let timecodes = {};
-        let $list = $('ul[data-audio-file="'+source+'"]');
-        let $items = $('li', $list);
+// referenced: https://codepen.io/patriferra/pen/bGdNxLM
+$('audio.with-transcript').each(function() {
+    let $audio_el = $(this);
+    let source = $audio_el.find('source').attr('src');    
+    let timecodes = {};
+    let $list = $('ul[data-audio-file="'+source+'"]');
+    let $items = $('li', $list);
 
-        $items.each(function() {
-            // get timecode from data attribute
-            let timecode = $(this).attr('data-timecode');
-            // build object of timecodes and element offsets
-            timecodes[timecode] = {};
-            timecodes[timecode]['el'] = $(this);
-            timecodes[timecode]['offset'] = $(this).position().top - $list.position().top;
+    $items.each(function() {
 
-            // Add click event to seek to the specific timecode
-            $(this).on('click', function() {
-                let timeparts = timecode.split(':');
-                let seconds = calc_seconds(timeparts);
-                $audio_el[0].currentTime = seconds;
-                $audio_el[0].play();
-            });
-        });
+        // get timecode from data attribute
+        let timecode = $(this).attr('data-timecode');
 
-        // Define mediaelement.js player
-        $audio_el.mediaelementplayer({
-            features: ['playpause', 'current', 'progress', 'duration'],
-            stretching: 'responsive'
-        });
+        // build object of timecodes and element offsets
+        timecodes[timecode] = {};
+        timecodes[timecode]['el'] = $(this);
+        timecodes[timecode]['offset'] = $(this).position().top;
 
-        // Set up the scroll tether to audio timecode
-        $audio_el.on('canplaythrough', function() {
-            // console.log('can play through!');
-        }).bind('timeupdate', function() {
-            let current_time = $audio_el[0].currentTime;
-            let current_minutes = formatNumber(parseInt(current_time / 60, 10));
-            let current_seconds = formatNumber(parseInt(current_time % 60));
-
-            let timecode_index = current_minutes + ":" + current_seconds;
-            // console.log(current_minutes + ":" + current_seconds);    
-            if (typeof timecodes[timecode_index] !== 'undefined') {
-                if (!timecodes[timecode_index]['el'].hasClass('current')) {
-                    $('.current', $list).removeClass('current');
-                    timecodes[timecode_index]['el'].addClass('current');
-                    // Scroll the ul element to the next timecode position proactively
-                    let next_timecode_index = getNextTimecodeIndex(current_minutes, current_seconds, timecodes);
-                    if (next_timecode_index) {
-                        $list.stop().animate({
-                            scrollTop: timecodes[next_timecode_index]['offset']
-                        }, 500);
-                    }
-                }            
-            }
+        // Add click event to take user to the specific timecode
+        $(this).on('click', function() {
+            let timeparts = timecode.split(':');
+            let seconds = calc_seconds(timeparts);
+            $audio_el[0].currentTime = seconds;
+            $audio_el[0].play();
         });
     });
 
-    function formatNumber(n) {
-        return n > 9 ? "" + n : "0" + n;
-    }
+    // Define mediaelement.js player
+    $audio_el.mediaelementplayer({
+        features: ['playpause','current','progress','duration'],
+        // features: ['playpause','current','progress','duration','volume','fasterslower'],
+        stretching: 'responsive',
+    });
 
-    function calc_seconds(array) {
-        let sum = 0;
-        switch(array.length) {
-            case 1:
-                sum += array[0];
-                break;
-            case 2:
-                sum += (array[0] * 60);
-                sum += array[1];
-                break;
-            case 3:
-                sum += (array[0] * 60 * 60);
-                sum += (array[1] * 60);
-                sum += array[2];
-                break;
+    // Set up the scroll tether to audio timecode
+    $audio_el.on('canplaythrough', function() {
+        // console.log('can play through!');
+    }).on('timeupdate', function() {
+        let current_time = $audio_el[0].currentTime;
+        let current_minutes = formatNumber(parseInt(current_time / 60, 10));
+        let current_seconds = formatNumber(parseInt(current_time % 60));
+
+        let timecode_index = current_minutes + ":" + current_seconds;
+        // console.log(current_minutes + ":" + current_seconds);    
+        // console.log($('li[data-timecode="'+timecode+'"]').length + 'matching els');
+        if (typeof timecodes[timecode_index] !== 'undefined') {
+            if(!timecodes[timecode_index]['el'].hasClass('current')) {
+                $('.current', $list).removeClass('current');
+                timecodes[timecode_index]['el'].addClass('current');
+                // Scroll the ul element to the next timecode position proactively
+                let newScrollTop = timecodes[timecode_index]['el'].position().top + $list.scrollTop();
+                $list.stop().animate({
+                    scrollTop: newScrollTop
+                }, 500);
+
+                // Scroll only when need to at cutoff point
+                // let newScrollTop = timecodes[timecode_index]['el'].position().top + $list.scrollTop() - $list.position().top;
+                // $list.stop().animate({
+                //     scrollTop: newScrollTop
+                // }, 500);
+
+            }            
         }
-        return sum;
-    }
-
-    function getNextTimecodeIndex(current_minutes, current_seconds, timecodes) {
-        let found = false;
-        let next_timecode_index = null;
-        Object.keys(timecodes).forEach((timecode) => {
-            if (found) {
-                next_timecode_index = timecode;
-                found = false;
-            }
-            if (timecode === current_minutes + ":" + current_seconds) {
-                found = true;
-            }
-        });
-        return next_timecode_index;
-    }
+    });
 });
+
+function formatNumber(n) {
+    return n > 9 ? "" + n : "0" + n;
+}
+
+function calc_seconds(array) {
+    let sum = 0;
+    switch(array.length) {
+        case 1:
+            sum += array[0];
+            break;
+        case 2:
+            sum += (array[0]*60);
+            sum += array[1];
+            break;
+        case 3:
+            sum += (array[0]*60*60);
+            sum += (array[1]*60);
+            sum += array[2];
+            break;
+    }
+    return sum;
+}
