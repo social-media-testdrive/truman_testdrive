7e4aa41b06facad068c378c0e5e3a149d77e4bdf
theophilaabigail <122048057+theophilaabigail@users.noreply.github.com>
2024-07-18 00:02:42 +0700

    Cyberbullying module partically complete. Still waiting on some missing texts.

---
+++ public2/module_js/cyberbullying-esp/cyberbullying-esp_tutorial.js
@@ -0,0 +1,61 @@
+const nextPageURL = 'tut_guide';
+
+const stepsList = [
+  {
+    element: '#step1',
+    intro: `¡Dale clic a “Siguiente” para comenzar!`,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['']
+  },
+  {
+    element: '#step1',
+    intro: `Recientemente has visto que unos compañeros de la escuela están acosando a Dylan en las redes sociales.`,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.1.mp3']
+  },
+  {
+    element: '#step1',
+    intro: `El <b>ciberbullying</b> se da cuando alguien publica o comparte cosas negativas sobre otra persona en línea. El acosador puede usar dispositivos digitales, sitios o aplicaciones. Este suele hacer esto una y otra vez a la misma persona.
+    `,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.2.mp3']
+  },
+  {
+    element: '#step3.text',
+    intro: `Este es un ejemplo de <b>ciberbullying.</b>`,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.3.mp3']
+  },
+  {
+    element: 'a.flag',
+    intro: `Una forma de actuar contra el ciberbullying es dándole clic al botón <b>"Reportar"</b>.`,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.4.mp3']
+  },
+  {
+    element: 'a.flag',
+    intro: `<b>Reportar</b> la publicación al sitio web puede llevar a que se elimine la publicación despectiva. A veces, el sitio web puede tardar un tiempo en hacerlo.`,
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.5.mp3']
+  },
+  {
+    element: '#step7',
+    intro: `Otra forma de actuar contra el ciberbullying es escribiendo <b>un comentario de apoyo</b> a la víctima.`,
+    position: 'left',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.6.mp3']
+  },
+  {
+    element: '#step7',
+    intro: `Otra forma de actuar contra el ciberbullying es <b>enfrentarse al acosador.</b>`,
+    position: 'left',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.7.mp3']
+  }
+];
